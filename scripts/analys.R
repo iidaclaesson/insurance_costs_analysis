887831a3f186230b6138eb57b8d6e2f134a6bd5d
iidaclaesson <iidaclaesson@gmail.com>
2026-04-25 18:30:41 +0800

    "Fixed analysis"

--- scripts/analys.R
+++ scripts/analys.R
@@ -3,9 +3,9 @@
 # Fördelning av kostnaderna
 
 ggplot(insurance_costs_clean, aes(x = charges)) +
-  geom_histogram(fill = "steelblue", bins = 40)
+  geom_histogram(fill = "steelblue", bins = 20)
 
-#TOLKNING
+# Histogramet visar att försäkringskostnaderna för de flesta ligger runt 10000kr, dock har en del kunder en högre kostnad.
 
 
 # Samband mellan ålder och kostnad
@@ -48,4 +48,51 @@ p_bmi_charge
 
 # correlation = 0.152
 # Sambandet mellan BMI och kostnader är positivt, vilket tyder på att ett högre BMI värde kan vara kopplat till högre kostnader
+
+
+
+# Skillnad mellan BMI grupper
+
+insurance_costs_clean %>%
+  group_by(bmi_category) %>%
+  summarise(avg_cost = mean(charges, na.rm = TRUE))
+
+p_bmi_groups <- ggplot(insurance_costs_clean, aes(x = bmi_category, y = charges)) +
+  geom_boxplot(alpha = 0.7) +
+  labs(
+    title = "Kostnad för BMI-kategorier",
+    x = "BMI",
+    y = "Kostnad"
+    ) +
+  coord_flip() +
+  theme_minimal() 
+
+p_bmi_groups
+
+
+# Median för grupperna verkar öka i samband med BMI-kategori.
+# För grupperna Overweight och Obese är kostnaderna större än för resterande grupper med median kostnad 10867kr respektive 10119kr, som tyder på att BMI-grupper kan påverka försäkringskostnader.
+
+
+
+# Skillnader mellan rökare och icke-rökare
+insurance_costs_clean %>%
+  group_by(smoker) %>%
+  summarise(avg_cost = mean(charges, na.rm = TRUE))
+
+
+p_smoker_cost <- ggplot(insurance_costs_clean, aes(x =smoker, y =charges)) +
+  geom_boxplot(alpha = 0.7) +
+  labs(
+    title = "Försäkringskostnad för rökare vs icke-rökare",
+    x = "Rökare",
+    y = "Kostnad"
+  ) +
+  coord_flip() +
+  theme_minimal() 
+
+p_smoker_cost
+
+# Medianen för försäkringskostnader för de som röker verkar dubbelt så stor som för icke-rökare som har en kostnad på 8586kr och för rökare 16537kr.
+# Boxploten visar tydligt att försäkringskostnaden är högre för rökare än för icke-rökare som tyder på att rökning har en betydande roll
 
