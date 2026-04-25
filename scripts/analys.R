b41416835d0a6cb46b4f6cd0ce4150245f7a16c4
iidaclaesson <iidaclaesson@gmail.com>
2026-04-25 13:53:21 +0800

    "Samband mellan bmi och kostnad"

--- scripts/analys.R
+++ scripts/analys.R
@@ -27,3 +27,25 @@ p_age_charge
 
 # correlation=0.258
 # Finns ett positivt samband mellan ålder och kostnader, som visar att äldre har högre försäkringskostnader
+
+
+# Samband mellan BMI och kostnad 
+
+insurance_costs_clean %>%
+  summarise(correlation = cor(bmi, charges, use = "complete.obs"))
+
+p_bmi_charge <- ggplot(insurance_costs_clean, aes(x = bmi, y = charges)) +
+  geom_point(alpha = 0.5, color = "steelblue") +
+  geom_smooth(method = "lm", se = TRUE) +
+  labs(
+    title = "Samband mellan BMI och kostnad",
+    x = "BMI",
+    y = "Kostnad"
+  ) +
+  theme_minimal()
+
+p_bmi_charge
+
+# correlation = 0.152
+# Sambandet mellan BMI och kostnader är positivt, vilket tyder på att ett högre BMI värde kan vara kopplat till högre kostnader
+
